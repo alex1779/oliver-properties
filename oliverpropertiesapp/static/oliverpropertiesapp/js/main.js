7437271e232e76235410d09eaa6add3c7fac1282
alex1779 <alex1779@gmail.com>
2024-03-26 16:28:50 -0300

    update

--- oliverpropertiesapp/static/oliverpropertiesapp/js/main.js
+++ oliverpropertiesapp/static/oliverpropertiesapp/js/main.js
@@ -10,9 +10,8 @@ menu.addEventListener("mouseleave", (event) => {
 });
 
 
-let menu2 = document.querySelector('.submenu_dropdown2')
 let submenu = document.querySelector('.submenu')
-
+let menu2 = document.querySelector('.submenu_dropdown2')
 
 submenu.addEventListener("mouseover", (event) => {
     menu2.style.display = "block"
@@ -28,7 +27,7 @@ let submenu_dropdown_casas = document.querySelector('.submenu_dropdown_casas')
 
 submenu_casas.addEventListener("mouseover", (event) => {
     submenu_dropdown_casas.style.display = "block"
-    console.log("hola")
+    //console.log("hola")
 });
 
 submenu_casas.addEventListener("mouseleave", (event) => {
@@ -45,4 +44,28 @@ menu_rent.addEventListener("mouseover", (event) => {
 
 menu_rent.addEventListener("mouseleave", (event) => {
     submenu_dropdown_rent.style.display = "none"
+});
+
+
+let rent_submenu_departamentos = document.querySelector('#rent_submenu_departamentos');
+let rent_submenu_departamentos_items = document.querySelector('#rent_submenu_departamentos_items')
+
+rent_submenu_departamentos.addEventListener("mouseover", (event) => {
+    rent_submenu_departamentos_items.style.display = "block"
+});
+
+rent_submenu_departamentos.addEventListener("mouseleave", (event) => {
+    rent_submenu_departamentos_items.style.display = "none"
+});
+
+
+let rent_submenu_casas = document.querySelector('#rent_submenu_casas');
+let rent_submenu_casas_items = document.querySelector('#rent_submenu_casas_items')
+
+rent_submenu_casas.addEventListener("mouseover", (event) => {
+    rent_submenu_casas_items.style.display = "block"
+});
+
+rent_submenu_casas.addEventListener("mouseleave", (event) => {
+    rent_submenu_casas_items.style.display = "none"
 });
